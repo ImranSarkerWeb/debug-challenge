77a9168383809f59ca8b5e17dbccf4e7d4529565
imransarkerweb <imransarker.web@gmail.com>
2023-03-08 11:19:50 +0600

    add button fix

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
+   updatePrice('price', price);
 
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
@@ -87,20 +87,20 @@ const setInnerText = (id, value) => {
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
    const priceConverted = getInputValue('price');
-   if (priceConverted > 200) {
-      setInnerText('delivery-charge', 30);
-      setInnerText('total-tax', priceConverted * 0.2);
-   }
-   if (priceConverted > 400) {
-      setInnerText('delivery-charge', 50);
-      setInnerText('total-tax', priceConverted * 0.3);
-   }
    if (priceConverted > 500) {
       setInnerText('delivery-charge', 60);
       setInnerText('total-tax', priceConverted * 0.4);
    }
-};
 
+else if (priceConverted > 400) {
+   setInnerText('delivery-charge', 50);
+   setInnerText('total-tax', priceConverted * 0.3);
+}
+  else if (priceConverted > 200) {
+      setInnerText('delivery-charge', 30);
+      setInnerText('total-tax', priceConverted * 0.2);
+   }
+};
 //grandTotal update function
 const updateTotal = () => {
    const grandTotal =
